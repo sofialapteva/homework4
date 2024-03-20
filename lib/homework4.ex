150018a9a36c0e4ad84aea2fb1b66bb3d11bd14b
Your Name <sofia_lapteva@mail.ru>
2024-03-20 17:15:58 +0300

    init

---
+++ lib/homework4.ex
@@ -0,0 +1,18 @@
+defmodule Homework4 do
+  @moduledoc """
+  Documentation for `Homework4`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Homework4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/homework4_test.exs
@@ -0,0 +1,8 @@
+defmodule Homework4Test do
+  use ExUnit.Case
+  doctest Homework4
+
+  test "greets the world" do
+    assert Homework4.hello() == :world
+  end
+end
